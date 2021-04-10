3228f99e7a4cffb5610771dc109c43edb08cd331
Alter <69980607+AlterRainbow@users.noreply.github.com>
2021-04-10 21:55:13 +0200

    Update The Maze.lua

--- The Maze.lua
+++ The Maze.lua
@@ -1,6 +1,6 @@
 local runsrvc = game:GetService("RunService")
 
-local alterlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlterRainbow/UI-Library/main/UILib.lua.lua"))()
+local alterlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlterRainbow/UI-Library/main/UILib.lua"))()
 
 local main = alterlib:Window("The Maze")
 
