51b09fe9619c18aaf910b2d636b121541dbf79ec
Alter <69980607+AlterRainbow@users.noreply.github.com>
2021-04-17 04:02:58 +0200

    Update The Maze.lua

--- The Maze.lua
+++ The Maze.lua
@@ -2,6 +2,74 @@ if not syn then
     game.Players.LocalPlayer:Kick("Dm me if you got protosmasher.")
 end
 
+local function Chams(parents, parts, color, onlyPlayers)
+    local localPlayer = game.Players.LocalPlayer
+    local parents = parents or {localPlayer.Name}
+	local parts = parts or {"HumanoidRootPart"}
+	local color = color or Color3.fromRGB(62, 74, 88)
+	local onlyPlayers = onlyPlayers or false
+    local faces = {"Front", "Top", "Back", "Bottom", "Right", "Left"}
+    local instances = {}
+	
+	if onlyPlayers then
+        for playerIndex, player in pairs(game.Players:GetPlayers()) do
+            if player ~= localPlayer then
+                for playerDescendantIndex, playerDescendant in pairs(player.Character:GetDescendants()) do
+                    if table.find(parts, playerDescendant.Name) then
+                        for faceIndex, face in pairs(faces) do
+                            local surface = Instance.new("SurfaceGui", playerDescendant)
+                            local frame = Instance.new("Frame", surface)
+                
+                            surface.Adornee = playerDescendant
+                            surface.AlwaysOnTop = true
+                            surface.Face = Enum.NormalId[face]
+                            surface.Name = face
+                            surface.ResetOnSpawn = false
+                
+                            frame.BackgroundColor3 = color
+                            frame.BackgroundTransparency = 0.6
+                            frame.Name = face
+                            frame.Size = UDim2.new(1, 0, 1, 0)
+                
+                            table.insert(instances, surface)
+                        end
+                    end
+                end
+            end
+        end
+    else
+        for descendantIndex, descendant in pairs(game.Workspace:GetDescendants()) do
+            if descendant:IsA("Part") and table.find(parts, descendant.Name) then
+                for parentIndex, parent in pairs(parents) do
+                    if parent == descendant.Parent.Name then
+                        if table.find(parts, descendant.Name) then
+                            for faceIndex, face in pairs(faces) do
+                                local surface = Instance.new("SurfaceGui", descendant)
+                                local frame = Instance.new("Frame", surface)
+                        
+                                surface.Adornee = descendant
+                                surface.AlwaysOnTop = true
+                                surface.Face = Enum.NormalId[face]
+                                surface.Name = face
+                                surface.ResetOnSpawn = false
+                    
+                                frame.BackgroundColor3 = color
+                                frame.BackgroundTransparency = 0.6
+                                frame.Name = face
+                                frame.Size = UDim2.new(1, 0, 1, 0)
+                        
+                                table.insert(instances, surface)
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    return instances
+end
+
 local runsrvc = game:GetService("RunService")
 
 local Alterlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlterRainbow/UI-Library/main/UILib.lua"))()
@@ -11,6 +79,38 @@ local main = Alterlib:Window("The Maze")
 local tab = main:Tab("Items")
 
 local plrTAB = main:Tab("Local")
+
+local monsters = main:Tab("Monsters")
+
+local togglecrajol = monsters:Toggle("The Crajoler Chams", function(crajol)
+    local crajolparents = {"TheCajoler"}
+    local crajolparts = {"HumanoidRootPart"}
+    
+    local see = crajol
+    
+    if see == true then
+        crajolchams = Chams(crajolparents, crajolparts, Color3.fromRGB(255, 0, 0), false)
+    elseif see == false then
+        for crajolchamindex, crajolcham in pairs(crajolchams) do
+            crajolcham:destroy()
+        end
+    end
+end)
+
+local toggleorto = monsters:Toggle("The Orotund Chams", function(orotund)
+    local ortoparents = {"TheOrotund"}
+    local ortoparts = {"HumanoidRootPart"}
+    
+    local chamm = orotund
+    
+    if chamm == true then
+        ortochams = Chams(ortoparents, ortoparts, Color3.fromRGB(255, 0, 0), false)
+    elseif chamm == false then
+        for ortochamindex, ortocham in pairs(ortochams) do
+            ortocham:destroy()
+        end
+    end
+end)
 
 local welksped = plrTAB:Slider("Speed", 0, 120, 16, function(davalue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = davalue
