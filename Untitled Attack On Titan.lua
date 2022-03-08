ace843ede3281bb2a67e55bc382bf47c5b9182c8
Alter <69980607+AlterRainbow@users.noreply.github.com>
2022-03-08 20:19:41 +0100

    Create Untitled Attack On Titan.lua

---
+++ Untitled Attack On Titan.lua
@@ -0,0 +1,23 @@
+local mt = getrawmetatable(game)
+local nc = mt.__namecall
+
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    if getnamecallmethod() == "FireServer" then
+        if self.Name == "GasDepleteEvent" then
+            return
+        end
+        
+        if self.Name == "BladeDurEvent" then
+            return
+        end
+    end
+    
+    return nc(self, unpack(args))
+end)
+
+setreadonly(mt, true)
+
+-- infinite gas and infinite blade durability
+-- Alter#6089
